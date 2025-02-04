fbe8555851342f5716b752ee7f70f940639a1065
kjura <30084359+kjura@users.noreply.github.com>
2025-02-05 00:09:08 +0100

    feat(radical-rust): fix 4.0 / PI for fourier partial sum, add function parameteres, add plotly support, fix off by one error when setting range 1..n instead of 1..n+1

--- radical_approach_to_analysis/radical-rust/src/main.rs
+++ radical_approach_to_analysis/radical-rust/src/main.rs
@@ -1,14 +1,43 @@
 use std::f64::consts::PI;
-
 use ndarray::linspace;
+use plotly::{common::Mode, Layout, Plot, Scatter};
 
-const PI_FACTOR: f64 = PI / 4.0;
+const PI_FACTOR: f64 = 4.0 / PI;
 
 fn main() {
     
-    let furie = fourier_partial_sum(4);
+    let domain_step_size: usize = 1000;
+    let lower_bound = -1.0;
+    let upper_bound: f64 = 3.0;
+    let n = 4;
 
+    // for i in 1..2 {
+
+    //     for x in [-1.0, 0.0, 1.0, 2.0, 3.0] {
+
+    //         println!("{}", PI_FACTOR * coefficient(i) * (cosine_coefficient(i) * x).cos())
+
+    //     }
+    // }
+
+    let furie = fourier_partial_sum(n, domain_step_size, lower_bound, upper_bound);
     println!("{:?}", furie);
+
+    let mut haha = vec![];
+    for i in linspace(lower_bound, upper_bound, domain_step_size){
+        haha.push(i);
+    };
+
+    let trace2 = Scatter::new(haha, furie)
+    .mode(Mode::Lines)
+    .name("Lines");
+
+    let plot_title = format!("Fourier partial sum for n={}", n);
+    let layout = Layout::new().title(plot_title);
+    let mut plot = Plot::new();
+    plot.add_trace(trace2);
+    plot.set_layout(layout);
+    plot.show();
 
 }
 
@@ -44,11 +73,11 @@ fn cosine_coefficient(n: u8) -> f64 {
     ((2.0 * n as f64) - 1.0) * PI  / 2.0
 }
 
-fn fourier_partial_sum(n: u8) -> Vec<f64> {
+fn fourier_partial_sum(n: u8, domain_step_size: usize, lower_bound: f64, upper_bound: f64) -> Vec<f64> {
     
     let mut output_matrix: Vec<Vec<f64>> = Vec::new();
-    for i in 1..n {
-        let domain= linspace(-1.0, 3.0, 50);
+    for i in 1..n+1 {
+        let domain= linspace(lower_bound, upper_bound, domain_step_size);
         let mut output = Vec::new();
         for x in domain {
             output.push(PI_FACTOR * coefficient(i) * (cosine_coefficient(i) * x).cos());
