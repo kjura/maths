d7d5c33e78e390b00e8aea2edb2caaa4bd20ba33
kjura <30084359+kjura@users.noreply.github.com>
2025-10-13 18:34:43 +0200

    bressoud_radical_analysis: add notes for Chapter 1 (experimenting with notes)

--- radical_approach_to_analysis/Chapter1/Code/julia_code/Bressoud/src/Bressoud.jl
+++ radical_approach_to_analysis/Chapter1/Code/julia_code/Bressoud/src/Bressoud.jl
@@ -2,6 +2,10 @@ module Bressoud
 
 using Plots
 
+# julia --project=.
+
+include("ChapterOneNotes/archimidean_understanding.jl")
+export total_area_of_a_triangle
 
 
 end # module Bressoud
---
+++ radical_approach_to_analysis/Chapter1/Code/julia_code/Bressoud/src/ChapterOneNotes/archimidean_understanding.jl
@@ -0,0 +1,11 @@
+# export fourier_series, approach_left, approach_right, value_at_x, plot_fourier
+# include("ChapterOne/exercise_1_2_6.jl")
+# julia --project=.
+
+
+function total_area_of_a_triangle(n)
+    sum = 0
+    for i in n
+        sum += 
+    end
+end
