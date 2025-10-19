087bb6543d613b5923981885cd2c623f71c800b9
kjura <30084359+kjura@users.noreply.github.com>
2025-10-19 14:47:35 +0200

    bressoud_radical_analysis(code): Chapter 2 Exercise 2.1.1 fix wrong number of iterations for vertices

--- radical_approach_to_analysis/Code/julia_code/Bressoud/src/ChapterTwoExercises/exercise_2_1_1.jl
+++ radical_approach_to_analysis/Code/julia_code/Bressoud/src/ChapterTwoExercises/exercise_2_1_1.jl
@@ -3,7 +3,7 @@
 # export generate_numerical_vertices, generate_vertices, create_kth_vertex, plot_vertices
 
 function generate_numerical_vertices(n)
-    for k in 0:(2^n)
+    for k in 0:(2 * 2^n)
         println("($(-1 + k * 2.0^(-n)), $(1 - (-1 + k * 2.0^(-n))^(2)))")
     end
 end
@@ -14,7 +14,7 @@ function create_kth_vertex(k, n)
 end
 
 function generate_vertices(n)
-    for k in 0:(2^n)
+    for k in 0:(2 * 2^n)
         first_coordinate = "-1 + $(k) * 2^($(-n))"
         second_coordinate = "1 - (-1 + $(k) * 2^$(-n))^2"
         println("($(first_coordinate), $(second_coordinate))")
