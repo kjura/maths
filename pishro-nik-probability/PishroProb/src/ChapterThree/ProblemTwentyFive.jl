7a488d89eb5405af82a8d08c7c4355ab18f92239
kjura <30084359+kjura@users.noreply.github.com>
2026-08-09 18:31:05 +0200

    pishro-nik probability(code): add code for Chapter 3 Problem 25 (c)

--- pishro-nik-probability/PishroProb/src/ChapterThree/ProblemTwentyFive.jl
+++ pishro-nik-probability/PishroProb/src/ChapterThree/ProblemTwentyFive.jl
@@ -1,5 +1,7 @@
 # Pishornik Chapter Three Exercise 25
 
+# export median_geo, plot, plot_median_geo
+
 """
 a.
 """
@@ -14,4 +16,34 @@ function pmf_a(k)
     else
         return 0
     end
+end
+
+
+"""
+b.
+"""
+
+function median_geo(p, kind="ceiling")
+
+    if !(0 < p <= 1)
+       throw(DomainError(p, "Argument p must obey 0 < p <= 1")) 
+    end
+
+    if kind == "ceiling"
+        return 1 / log2(1 / (1 - p))
+    elseif kind == "floor"
+        return 1 + (1 / log2(1 / (1 - p)))
+    else
+        throw(ArgumentError("""kind can only accept two argumenst "ceiling" or "floor", but the function received "$(kind)\"""")) 
+    end
+end
+
+# plot_median_geo(range(0.1, 0.9, length=200))
+function plot_median_geo(f_domain)
+    y1 = median_geo.(f_domain, "ceiling")
+    y2 = median_geo.(f_domain, "floor")
+    
+    p1 = plot(f_domain, y1, title="1 / log2(1 / q)", xlabel="q", ylabel="Value")
+    p2 = plot(f_domain, y2, title="1 + (1 / log2(1 / q))", xlabel="q", ylabel="Value")
+    plot(p1, p2, layout=(1, 2), legend=false)
 end
--- pishro-nik-probability/PishroProb/src/PishroProb.jl
+++ pishro-nik-probability/PishroProb/src/PishroProb.jl
@@ -5,5 +5,6 @@ using Plots
 
 include("ChapterThree/ProblemTwentyFive.jl")
 
+export median_geo, plot, plot_median_geo
 
 end # module PishroProb
